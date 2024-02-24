1adcfb77d0890ccdf49db7b7c18864934684dcdd
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-02-24 17:24:40 +0900

    review

---
+++ Compare.java
@@ -0,0 +1,5 @@
+public class Compare {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Lotto.java
@@ -0,0 +1,21 @@
+public class Lotto {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        int[] lotto = new int[6];	// 로또 번호를 입력받을 배열을 선언해준다.
+
+        for(int i = 0; i < lotto.length; i++) {
+            int num = (int)(Math.random() * 44) + 1;	// 1~46까지의 임의의 수를 받는다.
+            lotto[i] = num;
+            for(int j = 0; j < i; j++) {	// 중복된 번호가 있으면 이전 포문으로 돌아가 다시 시행한다.
+                if(lotto[i] == lotto[j]) {
+                    i--;
+                    break;
+                }
+            }
+            System.out.print(lotto[i] + " ");
+        }
+
+    }
+}
