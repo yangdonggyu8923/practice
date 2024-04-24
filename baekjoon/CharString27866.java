3232eaaaef1a50c1364f7d71527167491742c988
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-04-24 23:17:30 +0900

    240424

---
+++ baekjoon/CharString27866.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class CharString27866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int i = sc.nextInt();
+        System.out.println(s.charAt(i-1));
+    }
+}
