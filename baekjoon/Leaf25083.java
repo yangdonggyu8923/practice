be7face82cea63ae93e931e88681723b6bd9a523
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-05-01 23:35:48 +0900

    240501

---
+++ baekjoon/Leaf25083.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+public class Leaf25083 {
+    public static void main(String[] args) {
+        String a = "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+        System.out.println(a);
+    }
+}
