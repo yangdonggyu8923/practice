543a27896974a2c1e9d79b2519b86387ae9c6c9a
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-05-15 22:53:16 +0900

    240515

---
+++ baekjoon/InputOutput11718.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class InputOutput11718 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()) {
+            String a = sc.nextLine();
+
+            System.out.println(a);
+        }
+        sc.close();
+    }
+}
