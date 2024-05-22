17517c4c7f82dfd22c822e79ee3405db7046e395
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-05-22 23:42:18 +0900

    240522

---
+++ baekjoon/APB11021.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class APB11021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+(i+1)+": "+ (a+b));
+        }
+    }
+}
