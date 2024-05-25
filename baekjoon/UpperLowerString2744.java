82b762f5b0e31d54e0dff7e137ffbce7d5bbad1d
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-05-25 23:25:28 +0900

    240525 baek 2417

---
+++ baekjoon/UpperLowerString2744.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class UpperLowerString2744 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String result = "";
+
+        for(int i=0; i<a.length(); i++){
+            char b = a.charAt(i);
+            if(Character.isUpperCase(b)){
+                result += Character.toLowerCase(b);
+            } else{
+                result += Character.toUpperCase(b);
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
