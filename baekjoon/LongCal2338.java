f885454834591bdb3cb12a249aab2834c855c933
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-05-26 23:13:33 +0900

    240526 baek 2338

---
+++ baekjoon/LongCal2338.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class LongCal2338 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        BigInteger a = in.nextBigInteger();
+        BigInteger b = in.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.print(a.multiply(b));
+    }
+}
