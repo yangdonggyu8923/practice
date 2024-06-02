48f330b28cb10da4116d3c70809e0fcb4e4dea6d
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-06-02 23:36:57 +0900

    baek 5337

---
+++ baekjoon/Welcome5337.java
@@ -0,0 +1,8 @@
+package baekjoon;
+public class Welcome5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
