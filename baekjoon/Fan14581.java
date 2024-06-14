fe632dca405e85245b211df2763f05fcef678ad3
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-06-14 22:01:37 +0900

    baek

---
+++ baekjoon/Fan14581.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Fan14581 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(":fan::fan::fan:\n" +
+                ":fan::"+id+"::fan:\n" +
+                ":fan::fan::fan:");
+    }
+}
