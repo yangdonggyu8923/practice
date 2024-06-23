d4654a0f89327b2317e0a4d18ab9468d50d2216f
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-06-23 23:29:21 +0900

    baek 1547

---
+++ baekjoon/Ball1547.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Ball1547 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int cup = 1;
+        for(int i=0; i<m; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (cup==x){
+                cup = y;
+            }
+            else if(cup==y){
+                cup = x;
+            }
+        }
+        System.out.println(cup);
+    }
+}
