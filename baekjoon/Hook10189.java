7110046b12bfe91703ca54a3d7ac0b670603469d
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-07-13 23:48:39 +0900

    baek 10189

---
+++ baekjoon/Hook10189.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+public class Hook10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
