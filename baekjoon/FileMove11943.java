c793459917a5765f90b2343853d22cbe08cd89da
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-07-14 23:31:29 +0900

    baek 11943

---
+++ baekjoon/FileMove11943.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class FileMove11943 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a1 = sc.nextInt();
+        int o1 = sc.nextInt();
+        int a2 = sc.nextInt();
+        int o2 = sc.nextInt();
+        System.out.println(Math.min(a1 + o2, a2 + o1));
+    }
+}
