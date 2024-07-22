ba7144eb437f2d7b5060a79c4b3bae917de1aa9f
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-07-22 22:57:47 +0900

    baek 13277

---
+++ baekjoon/BigInt13277.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigInt13277 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.multiply(b));
+    }
+}
