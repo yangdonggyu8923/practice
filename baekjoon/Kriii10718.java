551bc381c72245bb299285b615f1fa48fa39ee8a
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-08-04 22:29:23 +0900

    baek 11022

---
+++ baekjoon/Kriii10718.java
@@ -0,0 +1,8 @@
+package baekjoon;
+
+public class Kriii10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
