fd2a505e2c7df2f31a9ada5d39e2ab2d93bdb032
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-08-06 23:34:59 +0900

    baek 10699

---
+++ baekjoon/Today10699.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Today10699 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+        df.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println(df.format(date));
+    }
+}
