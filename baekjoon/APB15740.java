7597367b59807058e90d787586fefcfd594209cd
yangdonggyu8923 <yangdonggyu8923@gmail.com>
2024-08-09 23:10:21 +0900

    baek 15680

---
+++ baekjoon/APB15740.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class APB15740 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+    }
+}
---
+++ baekjoon/Yonsei15680.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Yonsei15680 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n == 0) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
